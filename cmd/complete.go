f5cbe4c79d68603d1432b849b44de7090cc48b95
zhuzongzhen <153462902@qq.com>
2021-07-31 22:52:40 +0800

    为scp添加简单的bash的tab补全

--- cmd/complete.go
+++ cmd/complete.go
@@ -8,31 +8,99 @@ var bash = `
 __dbg()
 {
 	[ "${SSHRCOMPLETE}" != "yes" ] && return
-	echo -e "-----"$@"-----\n"
+	echo -e "-----"$@"-----"
 }
 
-_conplete_sshr()
+_complete_scp()
+{
+	local tmp=($(cat ~/.sshr/sshr.conf | grep -v "^[ \t]*#" | awk '{print $1}'))
+	local uhost=()
+	for p in ${tmp[@]}; do
+		uhost=(${uhost[@]} $p":")
+	done
+
+	# 将输入按照空格切分
+	OIFS=${IFS}
+	IFS=' '
+	read -ra line <<<${COMP_LINE}
+	IFS=${OIFS}
+
+	#
+	local c=${#line[@]}
+	local dir=${line[$c-1]##*:}
+	[ $c == 2 ] && dir=""
+	local views=(${uhost[@]} $(compgen -o default -- ${dir}))
+
+	case $c in
+		2)	COMPREPLY=(${views[@]}) ;;
+		3)
+			if [ "${COMP_LINE: -1}" == " " ]; then
+				if [ x$(echo ${line[2]} | grep "@") == x ]; then
+					COMPREPLY=(${uhost[@]} $(compgen -o default))
+				else
+					COMPREPLY=($(compgen -o default))
+				fi
+				return
+			fi
+
+			for p in ${views[@]} ; do
+				if [ x$(echo $p | grep "@") != x ]; then
+					if [[ ${line[$c-1]} =~ ^"$p" ]]; then
+						COMPREPLY=($(compgen -o default -- ${dir}))
+						return
+					fi
+				fi
+
+				[[ $p =~ ^"${line[$c-1]}" ]] && COMPREPLY=(${COMPREPLY[@]} $p)
+			done
+		;;
+
+		4)
+			[ "${COMP_LINE: -1}" == " " ] && return
+			for p in ${views[@]} ; do
+				if [ x$(echo $p | grep "@") != x ]; then
+					if [[ ${line[$c-1]} =~ ^"$p" ]]; then
+						COMPREPLY=($(compgen -o default -- ${dir}))
+						return
+					fi
+				fi
+
+				[[ $p =~ ^"${line[$c-1]}" ]] && COMPREPLY=(${COMPREPLY[@]} $p)
+			done
+		;;
+
+	esac
+}
+
+_complete_sshr()
 {
 	local cmds=(%s)
-	local uhost=$(cat ~/.sshr/sshr.conf | grep -v "^[ \t]*#" | awk '{print $1}')
-	local views=(${cmds[@]} ${uhost})
+	local uhost=($(cat ~/.sshr/sshr.conf | grep -v "^[ \t]*#" | awk '{print $1}'))
+	local views=(${cmds[@]} ${uhost[@]})
 
-	__dbg ${#views[@]}  ${views[@]}
-	__dbg $1"--"$2"--"$3
+	COMPREPLY=()
 
-    COMPREPLY=()
+	__dbg ${COMP_CWORD} ${#COMP_WORDS[@]} ${COMP_WORDS[@]}
+	__dbg "\$1=$1,\$2=$2,\$3=$3"
+
     case "$3" in
     "$1")
         [ "$2" ] || { COMPREPLY=(${views[@]}) && return; }
-
-        for ((i = 0; i < ${#views[@]}; i++)); do
-			[[ ${views[i]} =~ ^"$2" ]] && COMPREPLY=(${COMPREPLY[@]} ${views[i]})
+		for p in ${views[@]}; do
+			[[ $p =~ ^"$2" ]] && COMPREPLY=(${COMPREPLY[@]} $p)
 		done
+		return
         ;;
     esac
+
+	# scp相关的complete
+	if [ "${COMP_WORDS[1]}" == "scp" ]; then
+		_complete_scp $@
+		return
+	fi
 }
 
-complete -F _conplete_sshr sshr
+complete  -F _complete_sshr sshr
 `
 
 func doComplete(c *cmd) error {
